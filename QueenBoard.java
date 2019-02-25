d38c98d52ba942590849b7069960c4fc675d21e7
Jude-Rizzo <jprizzo2020@gmail.com>
2019-02-25 11:20:44 -0500

    test good

--- QueenBoard.java
+++ QueenBoard.java
@@ -107,10 +107,26 @@ public class QueenBoard{
     return ans;
   }
 
+  public static void runTest(int i){
+    QueenBoard b;
+    int[]tests =   {1,2,3,4,5,8};
+    int[]answers = {1,0,0,2,10,92};
+    if(i >= 0 && i < tests.length ){
+      int size = tests[i];
+      int correct = answers[i];
+      b = new QueenBoard(size);
+      int ans  = b.countSolutions();
+
+      if(correct==ans){
+        System.out.println("PASS board size: "+tests[i]+" "+ans);
+      }else{
+        System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+      }
+    }
+  }
+
+
   public static void main(String[] args){
-    QueenBoard q = new QueenBoard(8);
-    System.out.println(q.countSolutions());
-    System.out.println(q.solve());
-    System.out.println(q);
+    runTest(0);
   }
 }
